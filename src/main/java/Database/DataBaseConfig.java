7ad22d6f88b2ce83113d55863895d218daa0cc7a
VitaliyJV <114652130+VitaliyJV@users.noreply.github.com>
2023-07-05 19:44:59 +0300

    2
    2

--- src/main/java/Database/DataBaseConfig.java
+++ src/main/java/Database/DataBaseConfig.java
@@ -2,7 +2,7 @@ package Database;
 
 public class DataBaseConfig {
 
-    public static final String JDBC_URL = "";
+    public static final String JDBC_URL = "jdbc:postgresql://35.238.176.199:5432/shop";
     public static final String USER = "dev12";
     public static final String PASSWORD = "dev12thebest";
 }
